cbd805edd43db366a09130175382dcbe42d067ea
Peter John Acklam <pjacklam@gmail.com>
2018-02-04 09:46:55 +0100

    import bignum 0.49
    git-cpan-module:   bignum
    git-cpan-version:  0.49
    git-cpan-authorid: PJACKLAM

--- lib/Math/BigFloat/Trace.pm
+++ lib/Math/BigFloat/Trace.pm
@@ -13,7 +13,7 @@ our ($accuracy, $precision, $round_mode, $div_scale);
 
 our @ISA = qw(Exporter Math::BigFloat);
 
-our $VERSION = '0.48';
+our $VERSION = '0.49';
 
 use overload;                   # inherit overload from Math::BigFloat
 
